f7bc8cdb3863916c0210ef9313f59c8728002ee9
Lucas Zarza <lucas-lsz@hotmail.com>
2020-09-15 21:49:10 -0300

    Finalizando app Quiz

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -42,17 +42,21 @@ const quizData = [
 ];
 
 const questionEl = document.getElementById("question");
+const quiz = document.getElementById("quiz");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
 const submitBtn = document.querySelector("button");
+const answers = document.querySelectorAll(".answer");
 
+let score = 0;
 let currentQuestion = 0;
 
 loadQuiz();
 
 function loadQuiz() {
+  clearAnswer();
   questionEl.innerHTML = quizData[currentQuestion].question;
   a_text.innerHTML = quizData[currentQuestion].a;
   b_text.innerHTML = quizData[currentQuestion].b;
@@ -61,9 +65,9 @@ function loadQuiz() {
 }
 
 function getSelectedAnswers() {
-  const answers = document.querySelectorAll(".answer");
   for (answer of answers) {
     if (answer.checked) {
+      console.log(answer.id);
       return answer.id;
     }
   }
@@ -71,15 +75,30 @@ function getSelectedAnswers() {
 }
 
 submitBtn.addEventListener("click", () => {
+  //check to the the answer, and get the return of the selected answers
   const answer = getSelectedAnswers();
 
+  //if the answer is null/false, will alert
   if (!answer) {
     alert("Please select an answer");
   } else {
+    //else, will see if there's another question, then increment the currentQuestion
+
+    if (answer === quizData[currentQuestion].correct) {
+      score++;
+    }
+
     currentQuestion++;
+    quizData[currentQuestion] != undefined
+      ? loadQuiz()
+      : (quiz.innerHTML =
+        `<h2>You get ${score}/${quizData.length} correct answers</h2>
+         <button onclick="location.reload()">Play Again</button>`);
   }
-
-  // quizData[currentQuestion] != undefined ? currentQuestion : getSelectedAnswers();
-
-  loadQuiz();
 });
+
+function clearAnswer() {
+  for (answer of answers) {
+    answer.checked ? (answer.checked = false) : answer.checked;
+  }
+}
