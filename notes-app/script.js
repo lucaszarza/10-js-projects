b44ffbcc1245e6ede46497b97be9e3cc58074302
Lucas Zarza <lucas-lsz@hotmail.com>
2020-09-21 21:27:05 -0300

    Adicionando javascript das notas

--- notes-app/script.js
+++ notes-app/script.js
@@ -1,17 +1,51 @@
-const notesEl = document.getElementById('notes')
-const editBtn = document.getElementById('edit')
-const deleteBtn = document.getElementById('delete')
+const addBtn = document.getElementById("add");
 
-const noteBody = notesEl.querySelector('.note-body')
-const noteTextArea = notesEl.querySelector('.note-edit-text')
+addBtn.addEventListener("click", () => {
+  addNewNote();
+});
 
-editBtn.addEventListener('click',()=>{
-  noteBody.classList.toggle('hidden')
-  noteTextArea.classList.toggle('hidden')
-})
+function addNewNote() {
+  const note = document.createElement("div");
+  note.classList.add("notes-container");
 
-noteTextArea.addEventListener('input', (e)=> {
-  const { value } = e.target;
-  
-  noteBody.innerHTML = marked(value);
-})
+  note.innerHTML = `
+    <div class="note-header">
+      <button id="edit"><i class="fas fa-edit"></i></button>
+      <button id="delete"><i class="fas fa-trash-alt"></i></button>
+    </div>
+  <div class="note-body hidden"></div>
+    <textarea class="note-edit-text"></textarea>
+  `
+  document.body.appendChild(note);
+
+  const editBtn = note.querySelector("#edit");
+  const deleteBtn = note.querySelector("#delete");
+
+  const noteBody = note.querySelector(".note-body");
+  const noteTextArea = note.querySelector(".note-edit-text");
+
+  editBtn.addEventListener("click", () => {
+    noteBody.classList.toggle("hidden");
+    noteTextArea.classList.toggle("hidden");
+  });
+
+  deleteBtn.addEventListener("click", () => {
+    note.remove();
+  });
+
+  noteTextArea.addEventListener("input", (e) => {
+    const { value } = e.target;
+    noteBody.innerHTML = marked(value);
+  });
+}
+
+{
+  /* <div id="notes" class="notes-container">
+<div class="note-header">
+  <button id="edit"><i class="fas fa-edit"></i></button>
+  <button id="delete"><i class="fas fa-t rash-alt"></i></button>
+</div>
+<div class="note-body hidden"></div>
+<textarea class="note-edit-text"></textarea>
+</div> */
+}
